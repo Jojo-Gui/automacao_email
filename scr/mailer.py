4eb77eb651d7f3130f5274cbf59d3e8158b3806b
Jojo_Gui <joaopedro.guidetti@gmail.com>
2026-06-18 18:55:43 -0300

    Finalização do mailer.py e teste de envio de e-mail (Sucesso!)

--- scr/mailer.py
+++ scr/mailer.py
@@ -1,3 +1,8 @@
+import os
+import smtplib
+from dotenv import load_dotenv
+from email.message import EmailMessage
+
 def gerar_html(mock):
 
     #Criando corpo do email
@@ -15,6 +20,31 @@ def gerar_html(mock):
     """
 
     return corpo_email
+
+def enviar_email(corpo_email, destinatario):
+
+    # Carrega dados do .env
+    load_dotenv()
+
+    # Guarda as credenciais/variáveis
+    email_remetente = os.getenv("EMAIL_REMETENTE")
+    senha_remetente = os.getenv("SENHA_APP")
+
+    # Configurações do email
+    msg = EmailMessage()
+    msg['Subject'] = 'Resumo dos Produtos Analisados'
+    msg['From'] = email_remetente
+    msg['To'] = destinatario
+
+    # Injetando o HTML
+    msg.set_content(corpo_email, subtype='html')
+
+    # Abre a conexão segura com o servidor do Google na porta 465
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_remetente, senha_remetente) # Faz o login
+        smtp.send_message(msg) # enviar email
+        print("E-mail enviado com sucesso!")
+
 
 if __name__ == "__main__":
     print("Iniciando o teste do módulo mailer.py...")
@@ -34,4 +64,15 @@ if __name__ == "__main__":
 
     # Gera HTML
     html = gerar_html(mock)
-    print(html)
+    
+    lista_de_emails = [
+        'sdayane577@gmail.com',
+        'leo.mlr06@gmail.com',
+        'jpguidetti2@hotmail.com'        
+    ]
+
+    # tranforma a lista em texto
+    lista_de_emails = ', '.join(lista_de_emails)
+
+    # Envia email
+    enviar_email(html, lista_de_emails)
